e10fce18015019b1511e60e01d406c01ea124d18
albertoconcovia <concovia@gmail.com>
2021-11-22 00:15:18 +0000

    Extension 3 - Interactivity added

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+function Form({catCount, dogCount}) {
+
+return (
+    <div className="form">
+        <h3 className="card__text card__header">Add yout favourite cats or dogs:</h3>
+
+    </div>
+  )
+}
+
+export default Form
